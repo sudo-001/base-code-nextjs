2cd1599b5113055fb1938a3bfb7311de3bd82996
sudo-001 <carick.atezong@facsciences-uy1.cm>
2026-03-25 13:05:20 +0100

    Initialisation code base nextJs: Ajout des outils : - Next.js (App Router) - TypeScript - TailwindCSS - shadcn/ui - Framer Motion Redux Toolkit (RTK) - Lucide Icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Provider } from "react-redux";
+import { store } from "@/store";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body>
+        <Provider store={store}>{children}</Provider>
+      </body>
     </html>
   );
 }
